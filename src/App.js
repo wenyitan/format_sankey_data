77471527e77aaa47bfd87377043cb2bbe55e68d2
wenyitan <tanwenyi1511@gmail.com>
2024-03-15 17:03:03 +0800

    refactored the adding and removing of data to their own function, also changed algorithm to update and remove data

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Flow from './components/Flow';
 import { Sankey, Tooltip } from 'recharts';
 
 function App() {
+
+  const [ extendedState, setExtendedState ] = useState({})
   // primary variable to be accessed by Sankey component
   const [ data, setData ] = useState();
 
@@ -23,9 +25,14 @@ function App() {
       .then((res)=>{
         setData(res.data.primary);
         setPrimaryData(res.data.primary)
-        console.log(res.data.primary);
+        // console.log(res.data.primary);
         setSecondaryData(res.data.secondary);
-        console.log(res.data.secondary);
+        // console.log(res.data.secondary);
+        // const tempExtendedState = {}
+        // Object.keys(res.data.secondary).forEach((key)=> {
+        //   tempExtendedState[key] = false;
+        // })
+        // setExtendedState(extendedState)
       })
       .catch((err)=> {
         console.log(err)
@@ -41,7 +48,6 @@ function App() {
   const getFlowData = () => {
     axios.get("http://localhost:8000/get-flow", {})
       .then((res)=>{
-        console.log(res.data.nodes)
         setNodes(res.data.nodes);
         setEdges(res.data.edges);
       })
@@ -56,7 +62,7 @@ function App() {
   }, [])
 
   const MyCustomNode = (data) => {
-    console.log(data)
+    // console.log(data)
     return (
       <g>
         <rect x={data.x} y={data.y} width={data.width} height={data.height} fill="rgb(81, 146, 202)" />
@@ -64,8 +70,82 @@ function App() {
       </g>
     )
   }
-  
-  /*
+
+  const addData = (originalData, target, index) => {
+    const secondaryNodes = secondaryData[target].nodes.slice(1)
+    const secondaryLinks = secondaryData[target].links
+    const primaryLinksLength = originalData.links.length
+    const linksToAppend = []
+    secondaryLinks.forEach((link)=> {
+      linksToAppend.push({"source" : index, "target": link.target + primaryLinksLength, "value":link.value})
+    })
+    // const newNodes = originalData.nodes.concat(secondaryNodes)
+    // const newLinks = originalData.links.concat(linksToAppend)
+    // newLinks.sort((link1, link2)=> {return link1.source - link2.source})
+    const newNodes = [];
+    const newLinks = [];
+    let nodeFound = false;
+    newNodes.push(originalData.nodes.at(0))
+    for (let originalIndex = 1; originalIndex < originalData.nodes.length; originalIndex++) {
+      const currentNode = originalData.nodes.at(originalIndex);
+      const currentLink = originalData.links.at(originalIndex-1);
+      if (!nodeFound) {
+        newNodes.push(currentNode);
+        newLinks.push(currentLink);
+        if (currentNode.name === target) {
+          for (let secondaryIndex = 0; secondaryIndex < secondaryNodes.length; secondaryIndex++) {
+            const secondaryNode = secondaryNodes.at(secondaryIndex);
+            const secondaryLink = secondaryLinks.at(secondaryIndex);
+            secondaryLink.source = index
+            secondaryLink.target = newNodes.length;
+            newLinks.push(secondaryLink)
+            newNodes.push(secondaryNode);
+          }
+          nodeFound = true;
+        }
+      } else {
+        if (currentLink.source !== 0 ){
+          currentLink.source += secondaryLinks.length
+        }
+        currentLink.target += secondaryLinks.length
+        newNodes.push(currentNode);
+        newLinks.push(currentLink);
+      }
+    }
+    const result = {"nodes": newNodes, "links": newLinks}
+    // console.log(result)
+    return result
+  }
+
+  const removeData = (data, target) => {
+    const toBeRemoved = secondaryData[target]
+    const nodesToBeRemoved = toBeRemoved.nodes.slice(1).map((node)=> {return node.name});
+    const indexTrack = []
+    const originalNodes = data.nodes;
+    const originalLinks = data.links;
+    for (let i = 0; i < originalNodes.length; i++) {
+      const node = originalNodes.at(i)
+      if (nodesToBeRemoved.includes(node.name)) {
+        indexTrack.push(i)
+      }
+    }
+    const newNodes = originalNodes.filter((node)=> {return !nodesToBeRemoved.includes(node.name)});
+    const newLinks = originalLinks.filter((link)=> {return !indexTrack.includes(link.target)});
+    newLinks.forEach((link)=> {
+      if (link.target > indexTrack.at(0)) {
+        link.target -= indexTrack.length;
+      }
+      if (link.source > indexTrack.at(0)) {
+        link.source -= indexTrack.length;
+      }
+    })
+    const result = {"nodes": newNodes, "links": newLinks}
+    // console.log(result)
+    return result
+
+  }
+
+    /*
    * handle click events on the sankey component.
    * each click events has a payload object which differes for clicks on a node and on a link
    * the payload object for links has a key called linkWidth. Function checks for this to determine whether the click is on a node or a link
@@ -75,17 +155,27 @@ function App() {
    * name of target has to be in the keys of secondaryData for there to be any change in the sankey data.
   */
   const handleClick = (e) => {
+    // console.log(e)
     let target;
+    let index;
     if (Object.keys(e).includes("linkWidth")) {
       target = e.payload.target.name;
+      index = e.index + 1;
     } else {
       if (e.payload.sourceNodes[0] === 0) {
         target = e.payload.name;
+        index = e.index;
       }
     }
     if (Object.keys(secondaryData).includes(target)) {
-      console.log("Key: " + target + " chosen, will handle change in chart data");
-      setData(secondaryData[target])
+      if (extendedState[target]) { // target is extended
+        setData(removeData(data, target));
+        extendedState[target] = false;
+      } else {
+        console.log("Key: " + target + " chosen, will handle change in chart data");
+        setData(addData(data, target, index))
+        extendedState[target] = true;
+      }
     }
   }
 
@@ -102,10 +192,10 @@ function App() {
       {data && 
         <Sankey
           width={1000}
-          height={1000}
+          height={1200}
           data={data}
           node={<MyCustomNode />}
-          nodePadding={50}
+          nodePadding={20}
           sort={false}
           margin={{
           left: 200,
--- src/components/Flow.js
+++ src/components/Flow.js
@@ -2,7 +2,7 @@ import ReactFlow, { Controls, Background } from 'reactflow';
 import 'reactflow/dist/style.css';
 
 function Flow({nodes, edges}) {
-    console.log(nodes)
+    // console.log(nodes)
   return (
     <div style={{ height: '100%' }}>
       <ReactFlow defaultNodes={nodes} defaultEdges={edges} fitView
